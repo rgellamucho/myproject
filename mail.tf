a49d19b9232cff0098a5b09c44b0f3d0dfa317c3
Romar Gellamucho <romar.gellamucho@mdi.net.ph>
2023-07-19 18:44:42 +0800

    yes

--- mail.tf
+++ mail.tf
@@ -1,4 +1,4 @@
 resource "local_file" "pet" {
-filename = "C:\MDI NOVARE_MAR\myproject\pets.txt"
-content = "We love pets!"
+  filename = "C:\MDI NOVARE_MAR\myproject\pets.txt"
+  content = "We love pets!"
 }
